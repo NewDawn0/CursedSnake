07f06c8aa217b5ccd12cd1253a32229ad9ccf5dd
NewDawn0 <aci.v.i.iv@gmail.com>
2023-12-06 16:22:39 +0100

    update src

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,27 @@
 // Includes
-#include "parser.h"
-#include "logger.h"
-#include <cstdlib>
-#include <SFML/Window.hpp>
+#include "util/parser.h"
+#include <iostream>
 
 // Main function
 int main (int argc, char** argv) {
-    parser::ArgParser parser;
-    parser.configure({"SSnake", "A scuffed snake game", 0.1f})
-        .add({{"--help", "-h"}, "false", true, false})
-        .add({{"--version", "-v"}, "false", true, false})
-        .add({{"--verbose", "-vb"}, "false", true, false})
-        .parse(argc, argv);
-    logger::LOG_INFO("Should not print : info");
-    logger::LOG_ERROR("Shoudl print : err");
-    logger::LOG_DEBUG("Shoudl print : debug");
+    util::Arg arg0({"--help", "-h"});
+    util::Arg arg1({"--version", "-v"});
+    util::Arg arg2({"--train", "-t"}, nullptr, true, true);
+    util::ArgParser parser = util::ArgParser()
+        .configure({"SSnake", "A scuffed snake game", 0.1f})
+        .add(arg0)
+        .add(arg1)
+        .add(arg2);
+    auto res = parser.get<int>("-h");
+    std::cout <<  parser.get<int>("-h").get() << std::endl;
+    if (res.isNone()) std::cout << "Value is none" << std::endl;
+    if (res.isSome()) std::cout << "Value is some : " << res.get() << std::endl;
+
+    // auto res = parser.getArg<int>("-t");
+    // auto dval = res.default_val.get();
+    // std::cout << dval << std::endl;
+    // logger::LOG_INFO("Should not print : info");
+    // logger::LOG_ERROR("Shoudl print : err");
+    // logger::LOG_DEBUG("Shoudl print : debug");
     return EXIT_SUCCESS;
 }
